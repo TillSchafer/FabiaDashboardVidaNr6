c563e713f6037368b1c54b44c5b4cfe66a3e326c
Till Schäfer <tschaefer@layer2.de>
2023-01-31 14:39:38 +0100

    funktioniert

--- FabiaDashboardVidaNr6/MauiProgram.cs
+++ FabiaDashboardVidaNr6/MauiProgram.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+﻿using Plugin.Firebase.Shared;
+using Plugin.Firebase.Auth;
+using Plugin.Firebase.Android;
+using Microsoft.Maui.LifecycleEvents;
 using FabiaDashboardVidaNr6.Data;
 
 namespace FabiaDashboardVidaNr6;
@@ -10,7 +13,8 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+            .RegisterFirebaseServices()
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
@@ -22,6 +26,23 @@ public static class MauiProgram
 		
 		builder.Services.AddSingleton<WeatherForecastService>();
 
+
 		return builder.Build();
 	}
+    private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+    {
+        builder.ConfigureLifecycleEvents(events =>
+        {
+            events.AddAndroid(android => android.OnCreate((activity, state) =>
+                CrossFirebase.Initialize(activity, state, CreateCrossFirebaseSettings())));
+        });
+
+        builder.Services.AddSingleton(_ => CrossFirebaseAuth.Current);
+        return builder;
+    }
+
+    private static CrossFirebaseSettings CreateCrossFirebaseSettings()
+    {
+        return new CrossFirebaseSettings(isAuthEnabled: true);
+    }
 }
